efd76fe43562cb2e995c9f2c8da9b7a51c91d91b
ismailBatoul001 <159812319+ismailBatoul001@users.noreply.github.com>
2025-12-09 17:30:19 -0500

    Ajout de la fonction et des ses tests pour la verifieer_disponibilite_equipement

--- plsql/fonctions_oper.sql
+++ plsql/fonctions_oper.sql
@@ -7,7 +7,7 @@ IS
   v_duree NUMBER;
 BEGIN
     SELECT date_debut, date_fin
-    INTO v_date_debut, v_date_fin;
+    INTO v_date_debut, v_date_fin
     FROM PROJET
     WHERE id_projet = p_id_projet
 
@@ -16,7 +16,56 @@ BEGIN
     RETURN v_duree;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20020, 'Projet inexistant' || SQLERRM);
+        RAISE_APPLICATION_ERROR(-20020, 'Projet inexistant');
     WHEN OTHERS THEN
         RAISE_APPLICATION_ERROR(-20999, 'Erreur calculer_duree_projet: ' || SQLERRM);
+END;
+
+CREATE OR REPLACE FUNCTION verifier_disponibilite_equipement(
+    p_id_equipement IN NUMBER
+) RETURN NUMBER
+IS
+    TYPE affect_rec IS RECORD (
+        id_affect        NUMBER,
+        id_projet        NUMBER,
+        date_affectation DATE,
+        duree_jours      NUMBER
+    );
+
+    TYPE affect_tab IS TABLE OF affect_rec;
+    v_affectations affect_tab;
+
+    v_etat VARCHAR2;
+    v_dispo_return NUMBER := 1;
+BEGIN
+    SELECT etat
+    INTO v_etat
+    FROM EQUIPEMENT
+    WHERE id_equipement = p_id_equipement;
+
+    IF v_etat != 'Disponible' THEN
+        v_dispo_return := 0;
+        RETURN v_dispo_return;
+    END IF;
+
+    SELECT id_affect, id_projet, date_affectation, duree_jours
+    BULK COLLECT INTO v_affectations
+    FROM AFFECTATION_EQUIP
+    WHERE id_equipement = p_id_equipement;
+
+    FOR i IN 1 .. v_affectations.COUNT LOOP
+        IF SYSDATE >= v_affectations(i).date_affectation
+        AND
+        SYSDATE <= (v_affectations(i).date_affectation + v_affectations(i).duree_jours)
+        THEN
+            v_dispo_return := 0;
+        END IF;
+    END LOOP;
+
+    RETURN v_dispo_return;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20021, 'Équipement inexistant');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20999, 'Erreur verifier_disponibilite_equipement: ' || SQLERRM);
 END;
--- tests/test_blocs.sql
+++ tests/test_blocs.sql
@@ -8,6 +8,7 @@ BEGIN
   VALUES (v_id_chercheur, 'BATOUL', 'ISMAIL', 'IA', SYSDATE);
 
   ajouter_projet(7001, 'Projet Test', 'IA', 10000, SYSDATE, SYSDATE+30, v_id_chercheur);
+  DBMS_OUTPUT.PUT_LINE('Projet ajouté avec succès.');
 
   ROLLBACK;
 END;
@@ -27,6 +28,7 @@ BEGIN
     VALUES (p_id_equipement, 'Equipement Test', 'Aventure', SYSDATE, 'Disponible');
 
     affecter_equipement(8001, p_id_projet, p_id_equipement, SYSDATE, 15);
+    DBMS_OUTPUT.PUT_LINE('Affectation de l''équipement réussie.');
 
     ROLLBACK;
 END;
@@ -43,7 +45,23 @@ BEGIN
   VALUES (v_id_projet, 'Projet Durée', 'Chimie', 15000, SYSDATE, SYSDATE+45, 1);
 
   v_duree := calculer_duree_projet(v_id_projet);
+  DBMS_OUTPUT.PUT_LINE('Durée du projet (en jours) : ' || v_duree);
 
   ROLLBACK;
 END;
 /
+
+--Bloc de test pour la fonction verifier_disponibilite_equipement
+SET SERVEROUTPUT ON;
+DECLARE
+  v_id_equipement NUMBER := 5101;
+  v_disponibilite NUMBER;
+BEGIN
+    INSERT INTO EQUIPEMENT (id_equipement, nom, categorie, date_acquisition, etat)
+    VALUES (v_id_equipement, 'Equipement Disponibilité', 'Biotech', SYSDATE, 'Disponible');
+
+    v_disponibilite := verifier_disponibilite_equipement(v_id_equipement);
+    DBMS_OUTPUT.PUT_LINE('Disponibilité avant affectation: ' || v_disponibilite);
+
+    ROLLBACK;
+    END;
